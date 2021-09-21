d37e72c0cd87e43e5012644663e9fd82ba40d604
evaleonsanchez <evamarialeonsanchez7@gmail.com>
2021-09-20 20:36:33 -0500

    proyecto del diplomado

---
+++ js/script.js
@@ -0,0 +1,77 @@
+$(".espa").mouseover(
+    function(){
+        $(".espa").css("background", "red")
+    }
+    
+    )
+    $(".espa").mouseout(
+        function(){
+            $(".espa").css("background", "white")
+        }
+        
+        )
+        
+
+
+
+
+
+
+
+
+
+
+
+        $(".jus").mouseover(
+            function(){
+                $(".jus").css("background", "black")
+            }
+            
+            )
+            $(".jus").mouseout(
+                function(){
+                    $(".jus").css("background", "white")
+                }
+                
+                )
+
+                $(".flexi").mouseover(
+                    function(){
+                        $(".flexi").css("background", "green")
+                    }
+                    
+                    )
+                    $(".flexi").mouseout(
+                        function(){
+                            $(".flexi").css("background", "white")
+                        }
+                        
+                        )
+
+
+                        $(".crea").mouseover(
+                            function(){
+                                $(".crea").css("background", "yellow")
+                            }
+                            
+                            )
+                            $(".crea").mouseout(
+                                function(){
+                                    $(".crea").css("background", "white")
+                                }
+                                
+                                )
+
+
+                                $(".conf").mouseover(
+                                    function(){
+                                        $(".conf").css("background", "blue")
+                                    }
+                                    
+                                    )
+                                    $(".conf").mouseout(
+                                        function(){
+                                            $(".conf").css("background", "white")
+                                        }
+                                        
+                                        )
